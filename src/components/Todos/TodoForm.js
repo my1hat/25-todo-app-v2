8c7d96cd3d9ae6cc95a4692d99d3072c47fe21ec
penuda <penuda@ya.ru>
2023-02-21 01:34:08 +0300

    first commit

---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styles from './TodoForm.module.css';
+import Button from '../UI/Button';
+
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState('');
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    addTodo(text);
+    setText('');
+  };
+  return (
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          placeholder="Enter new todo"
+        />
+        <Button type="submit" title="Submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
+export default TodoForm;
